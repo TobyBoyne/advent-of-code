0f82fb347353d862476ad0d838bb5dc4e76848fd
Toby Boyne <tobyboyne@gmail.com>
2020-03-21 13:42:13 +0000

    Day 1 Part 2

--- aoc-2015/2015-01.py
+++ aoc-2015/2015-01.py
@@ -7,5 +7,15 @@ def read_input():
 def part_one(s):
 	return s.count("(") - s.count(")")
 
+def part_two(s):
+	floor = 0
+	for idx, c in enumerate(s):
+		floor += 1 if c == "(" else -1
+		if floor == -1:
+			break
+
+	return idx + 1
+
 puzz_input = read_input()
 print(part_one(puzz_input))
+print(part_two(puzz_input))
