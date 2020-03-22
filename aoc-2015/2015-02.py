f85b70e3732dcfc0504b3035d93c9eb4b95668d2
Toby Boyne <tobyboyne@gmail.com>
2020-03-22 15:42:28 +0000

    Day 2 Part 2

--- aoc-2015/2015-02.py
+++ aoc-2015/2015-02.py
@@ -2,18 +2,27 @@ def read_input():
 	lengths = []
 	with open("day02.txt") as f:
 		for line in f:
-			lengths.append((int(l) for l in line.strip("\n").split("x")))
+			dimensions = [int(l) for l in sorted(line.strip("\n").split("x"))]
+			lengths.append(sorted(dimensions))
 
 	return lengths
 
 def area(dimensions):
-	l, w, h = sorted(dimensions)
+	l, w, h = dimensions
 	return 3 * l * w + 2 * w * h + 2 * l * h
+
+def ribbon(dimensions):
+	l, w, h = dimensions
+	return 2 * (l + w) + l * w * h
+
 
 def part_one(lengths):
 	return sum(area(l) for l in lengths)
 
+def part_two(lengths):
+	return sum(ribbon(l) for l in lengths)
 
 
 lengths = read_input()
 print(part_one(lengths))
+print(part_two(lengths))
