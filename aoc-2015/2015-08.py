a15812c81eb96251fbd43629af12022e59d5143b
Toby Boyne <tobyboyne@gmail.com>
2020-03-25 11:29:33 +0000

    Day 8 Part 2

--- aoc-2015/2015-08.py
+++ aoc-2015/2015-08.py
@@ -3,15 +3,26 @@ def read_file():
 		return [line.strip("\n") for line in f]
 
 
-def len_diff(s):
+def literal_len_diff(s):
 	escapes = s.count('\\\\')
 	s = s.replace('\\\\', ' ')
 	escapes += s.count('\\"') + s.count('\\x') * 3
-	return 2 + escapes
+	# +2 for the quote at the beginning/end
+	return escapes + 2
+
+def encoded_len_diff(s):
+	new_slashes = s.count('\\') + s[1:-1].count('"')
+	# +4 for quote at beginning/end
+	return new_slashes + 4
+
 
 def part_one(strings):
-	return sum(len_diff(s) for s in strings)
+	return sum(literal_len_diff(s) for s in strings)
+
+def part_two(strings):
+	return sum(encoded_len_diff(s) for s in strings)
 
 
 strings = read_file()
 print(part_one(strings))
+print(part_two(strings))
