63e62d26f04e3e1001a3912078503db227821233
Toby Boyne <tobyboyne@gmail.com>
2020-03-29 19:05:20 +0100

    Day 17 Part 2

--- aoc-2015/2015-17.py
+++ aoc-2015/2015-17.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 def read_input():
 	containers = []
 	with open("day17.txt") as f:
@@ -7,23 +5,42 @@ def read_input():
 			containers.append(int(line.strip("\n")))
 	return containers
 
-def num_combinations(containers, L):
+#  --- INITIAL SOLUTION ---
+# def num_combinations(containers, L):
+# 	# containers must be sorted
+# 	if L == 0:
+# 		return 1
+# 	total = 0
+# 	for i, c in enumerate(containers):
+# 		if c <= L:
+# 			remaining = containers[:i]
+# 			total += num_combinations(remaining, L - c)
+#
+# 	return total
+
+def find_all_combinations(containers, used, L):
 	# containers must be sorted
-	if L == 0:
-		return 1
-	total = 0
+	combinations = []
 	for i, c in enumerate(containers):
+		if c == L:
+			combinations.append(used + [c])
 		if c <= L:
 			remaining = containers[:i]
-			total += num_combinations(remaining, L - c)
+			combinations += find_all_combinations(remaining, used + [c], L - c)
 
-	return total
+	return combinations
 
-def part_one(containers):
-	containers.sort()
-	all_combinations = num_combinations(containers, 150)
 
-	return all_combinations
+def part_one(combinations):
+	return len(combinations)
+
+def part_two(combinations):
+	lengths = [len(comb) for comb in combinations]
+	min_length = min(lengths)
+	return sum(l==min_length for l in lengths)
+
 
 containers = read_input()
-print(part_one(containers))
+combinations = find_all_combinations(containers, [], 150)
+print(part_one(combinations))
+print(part_two(combinations))
