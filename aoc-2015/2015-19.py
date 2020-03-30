79382695f87cab8e70612748d79a64262744d1ee
Toby Boyne <tobyboyne@gmail.com>
2020-03-30 17:16:52 +0100

    Day 19 Part 2 Brute force attempt, takes too long to run - look at patterns of input

--- aoc-2015/2015-19.py
+++ aoc-2015/2015-19.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def read_input():
 	replacements = {}
 	with open("day19.txt") as f:
@@ -24,6 +26,28 @@ def molecule_to_list(molecule):
 	molecule_lst.append(cur_molecule)
 	return molecule_lst
 
+def list_is_subset(lst, sub):
+	for i in range(len(lst) - len(sub) + 1):
+		if sub == lst[i:i+len(sub)]:
+			return i
+	return -1
+
+def reduce(molecule_lst, origins, out_molecules, steps=0):
+	for mol in out_molecules:
+		idx = list_is_subset(molecule_lst, list(mol))
+		if idx != -1:
+			reduced_molecule_lst = molecule_lst[:idx] + [origins[mol]] + molecule_lst[idx + len(mol):]
+			if reduced_molecule_lst in (['e'], ['H', 'F'], ['N', 'Al'], ['O', 'Mg']):
+				print(steps, reduced_molecule_lst)
+				return True
+			else:
+				r = reduce(reduced_molecule_lst, origins, out_molecules, steps+1)
+				if r:
+					print("Double Done!")
+
+	return False
+
+
 def part_one(molecule, replacements):
 	molecule_lst = molecule_to_list(molecule)
 	new_molecules = []
@@ -37,5 +61,20 @@ def part_one(molecule, replacements):
 	unique_molecules = set(str_molecules)
 	return len(unique_molecules)
 
+def part_two(molecule, replacements):
+	molecule_lst = molecule_to_list(molecule)
+	# origins dict stores ways that an output molecule can be made - reverse of replacements
+	origins = {}
+	for k, v in replacements.items():
+		if k != 'e':
+			for mol in v:
+				origins[tuple(mol)] = k
+
+	out_molecules = list(sorted(origins.keys(), key=lambda x: -len(x)))
+
+	all_elements = [element for mol in out_molecules for element in mol]
+	reduce(molecule_lst, origins, out_molecules)
+
 molecule, replacements = read_input()
 print(part_one(molecule, replacements))
+print(part_two(molecule, replacements))
