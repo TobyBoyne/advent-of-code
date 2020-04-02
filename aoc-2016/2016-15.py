04544b5e99c33ddc411eff3ac271aae2209e0dd6
Toby Boyne <tobyboyne@gmail.com>
2020-04-02 14:15:32 +0100

    Day 15 Part 2

--- aoc-2016/2016-15.py
+++ aoc-2016/2016-15.py
@@ -24,6 +24,28 @@ def part_one(discs):
 		t += 1
 	return t
 
+def part_two(discs):
+	ts = []
+	t = 0
+	new_disc = (len(discs) + 1, 11)
+	new_disc_pos = []
+	while len(ts) < 2:
+		if lines_up(discs, t):
+			ts.append(t)
+			new_disc_pos.append((new_disc[0] + t) % new_disc[1])
+		t += 1
+
+	# treat the configuration as two discs: the combination of all previous discs, and the new disc
+	# simplify the phase difference between these and use it to find when they are next equal at pos=0
+	dt = ts[1] - ts[0]
+	dp = new_disc_pos[1] - new_disc_pos[0]
+	i = 0
+	while (new_disc_pos[0] + i * dp) % new_disc[1] != 0:
+		i += 1
+
+	T = ts[0] + i * dt
+	return T
+
 discs = read_input()
-print(discs)
 print(part_one(discs))
+print(part_two(discs))
