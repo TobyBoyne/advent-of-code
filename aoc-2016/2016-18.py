e22e01a6d2b05544acd9001d70176e8e0f3b7440
Toby Boyne <tobyboyne@gmail.com>
2020-04-04 17:34:26 +0100

    Day 18 Part 2 Improved next_line function to be ~20x more efficient using built in numpy functions

--- aoc-2016/2016-18.py
+++ aoc-2016/2016-18.py
@@ -1,4 +1,5 @@
 import numpy as np
+from time import perf_counter
 
 def read_input():
 	with open("day18.txt") as f:
@@ -6,13 +7,9 @@ def read_input():
 
 	return np.array([c == '^' for c in line])
 
-
 def next_line(prev_line):
 	next_row = np.zeros_like(prev_line)
-	for i in range(1, len(next_row) - 1):
-		# centre trap has no effect - return left XOR right
-		left, right = prev_line[i - 1], prev_line[i + 1]
-		next_row[i] = left != right
+	next_row[1:-1] = np.logical_xor(prev_line[:-2], prev_line[2:])
 	return next_row
 
 def part_one(first_row, n=40):
@@ -21,8 +18,13 @@ def part_one(first_row, n=40):
 	for i in range(n):
 		grid[i, :] = cur_row
 		cur_row = next_line(cur_row)
+
 	return np.count_nonzero(grid[:, 1:-1] == False)
 
 
 first_row = read_input()
 print(part_one(first_row))
+# Takes ~3.5seconds
+t = perf_counter()
+print(part_one(first_row, n=400000))
+print("Time taken =", perf_counter() - t)
