a685c77b6ba0001a2695029df2e177a25cbd62e2
Toby Boyne <tobyboyne@gmail.com>
2020-04-06 12:39:02 +0100

    Day 21 Part 1 - OOP

--- aoc-2016/2016-21.py
+++ aoc-2016/2016-21.py
@@ -1,14 +1,72 @@
 import numpy as np
 from parse import parse
 
+class Op:
+	def __init__(self, args):
+		self.args = args
+
+class SwapPosition(Op):
+	def __call__(self, arr, inverse=False):
+		# symmetric
+		arr[self.args[0]], arr[self.args[1]] = arr[self.args[1]], arr[self.args[0]]
+		return arr
+
+class SwapLetter(Op):
+	def __call__(self, arr, inverse=False):
+		# symmetric
+		i, j = np.argwhere(arr == self.args[0])[0], np.argwhere(arr == self.args[1])[0]
+		arr[i], arr[j] = arr[j], arr[i]
+		return arr
+
+class RotateSteps(Op):
+	def __call__(self, arr, inverse=False):
+		n = self.args[1]
+		n *= -1 if self.args[0] == "left" else 1
+		n *= -1 if inverse else 1
+		arr = np.roll(arr, n)
+		return arr
+
+class RotateBased(Op):
+	def __call__(self, arr, inverse=False):
+		if not inverse:
+			idx = np.argwhere(arr == self.args[0])[0]
+			n = 1 + idx + (idx >= 4)
+			arr = np.roll(arr, n)
+		else:
+			idx = np.argwhere(arr == self.args[0])[0][0]
+			i = 0
+			while not ((1 + idx == i) or ((2 + idx != i) and idx >= 4)):
+				arr = np.roll(arr, -1)
+				i += 1
+				idx = (idx - 1) % len(arr)
+				print(i, idx)
+		return arr
+
+class Reverse(Op):
+	def __call__(self, arr, inverse=False):
+		i, j = self.args
+		arr[i:j + 1] = arr[i:j + 1][::-1]
+		return arr
+
+class Move(Op):
+	def __call__(self, arr, inverse=False):
+		if not inverse:
+			i, j = self.args
+			value = arr[i]
+			arr = np.delete(arr, i)
+			arr = np.insert(arr, j, value)
+			return arr
+
+
+
 # unique word in string matches to a pattern for parse
 patterns = {
-	"swap position":	"swap position {:d} with position {:d}",
-	"swap letter":		"swap letter {} with letter {}",
-	"step":				"rotate {} {:d} {}",
-	"rotate based":		"rotate based on position of letter {}",
-	"reverse":			"reverse positions {:d} through {:d}",
-	"move":				"move position {:d} to position {:d}"
+	"swap position":	(SwapPosition, "swap position {:d} with position {:d}"),
+	"swap letter":		(SwapLetter, "swap letter {} with letter {}"),
+	"step":				(RotateSteps, "rotate {} {:d} {}"),
+	"rotate based":		(RotateBased, "rotate based on position of letter {}"),
+	"reverse":			(Reverse, "reverse positions {:d} through {:d}"),
+	"move":				(Move, "move position {:d} to position {:d}")
 }
 
 
@@ -16,40 +74,18 @@ def read_input():
 	moves = []
 	with open("day21.txt") as f:
 		for line in f:
-			for phrase, pat in patterns.items():
+			for phrase, details in patterns.items():
 				if phrase in line:
+					operation, pat = details
 					p = parse(pat, line.strip("\n"))
-					move = (phrase, p.fixed)
+					move = operation(p.fixed)
 					moves.append(move)
 	return moves
 
 def scramble(moves, password):
 	arr = np.array([c for c in password])
-	for move, args in moves:
-		print(arr, "next move", move, args)
-		if move == "swap position":
-			arr[args[0]], arr[args[1]] = arr[args[1]], arr[args[0]]
-		if move == "swap letter":
-			i, j = np.argwhere(arr == args[0])[0], np.argwhere(arr == args[1])[0]
-			arr[i], arr[j] = arr[j], arr[i]
-		if move == "step":
-			n = args[1]
-			n *= -1 if args[0] == "left" else 1
-			arr = np.roll(arr, n)
-		if move == "rotate based":
-			idx = np.argwhere(arr == args[0])[0]
-			n = 1 + idx + (idx >= 4)
-			arr = np.roll(arr, n)
-		if move == "reverse":
-			i, j = args
-			arr[i:j+1] = arr[i:j+1][::-1]
-		if move == "move":
-			i, j = args
-			if i < j:
-				arr = np.concatenate([arr[:i], arr[i+1:j+1], [arr[i]], arr[j+1:]])
-			else:
-				arr = np.concatenate((arr[:j], [arr[i]], arr[j:i], arr[i+1:]))
-
+	for move in moves:
+		arr = move(arr)
 	return arr
 
 def part_one(moves, password):
