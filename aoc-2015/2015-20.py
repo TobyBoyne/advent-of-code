e7919297b2626fd70d5fe556f96913f48869fcee
Toby Boyne <tobyboyne@gmail.com>
2020-04-06 19:05:42 +0100

    Day 20 Part 1

--- aoc-2015/2015-20.py
+++ aoc-2015/2015-20.py
@@ -1,33 +1,48 @@
 import numpy as np
+from time import perf_counter
 
 def primes(max_n=1000):
-	n = 2
-	while n <= max_n:
-		if all(n%i for i in range(2, n)):
-			yield n
-		n += 1
+	ns = np.arange(2, max_n)
+	primes = np.zeros_like(ns, dtype=bool)
+	for i, n in enumerate(ns):
+		if np.all(n % ns[:i]):
+			primes[i] = True
+	return ns[np.where(primes)]
+
 
 def num_presents(N):
 	elves = np.arange(1, N+1)
 	presents = elves * (N % elves == 0)
 	return 10 * sum(presents)
 
+def smallest_sufficient_multiplier(N):
+	"""Returns prime factors (excl. 2) that make up the smallest multiplier
+	e.g. x31 would return [3, 7]"""
 
 def part_one(target_presents):
-	N = target_presents / 10 - 1
-	initial_guess = 2 ** int(np.log2(target_presents / 10))
-	reduce_factor = target_presents/num_presents(initial_guess)
-	new_primes = []
+	N = target_presents
 
 
+
+scores = np.arange(2, 2*211).reshape(-1, 2) // 2
+for i, s in enumerate(scores):
+	scores[i, 1] = num_presents(s[1])
+
+print(scores)
+print(num_presents(120))
+for i in range(1, 1000):
+	if (score := num_presents(i)) > 5450:
+		print(i)
+		break
 
 x = 5 ** 5 * 3 ** 4
-print(num_presents(x) / num_presents(x * 3))
+x = 2 ** 9
+print(num_presents(x * 2 ** 7) / num_presents(x))
 """1/n+1 if n in prime factorisation, else 1/n"""
 
-
-puzz_input = 33100000
-print(part_one(puzz_input))
-
-x = 33100000 // 10
-print(np.log2(x))
+#
+# puzz_input = 33100000
+# print(part_one(puzz_input))
+#
+# x = 33100000 // 10
+# print(np.log2(x))
