7c1ae5f66c6302d80bee666607523a7f06d32ae5
Toby Boyne <tobyboyne@gmail.com>
2020-04-09 20:02:06 +0100

    Day 10 Part 2

--- aoc-2019/2019-10.py
+++ aoc-2019/2019-10.py
@@ -1,5 +1,5 @@
-import numpy as np
 from math import atan2
+from itertools import cycle
 
 def read_input():
 	asteroids = []
@@ -11,22 +11,49 @@ def read_input():
 	return asteroids
 
 def num_detected(station, all_asteroids):
-	in_sight = set()
+	in_sight = {}
 	stat_x, stat_y = station
 	for ast_x, ast_y in all_asteroids:
 		if (ast_x, ast_y) != station:
-			dy, dx = ast_y - stat_y, ast_x - stat_x
-			in_sight.add(atan2(dy, dx))
+			dy, dx = stat_y - ast_y, ast_x - stat_x
+			# rotate by 90 deg
+			dx, dy = -dy, dx
+			angle = - atan2(dy, dx)
+			d = abs(dx) + abs(dy)
+			in_sight[angle] = in_sight.get(angle, []) + [(d, ast_x, ast_y)]
 
-	return len(in_sight)
+	return in_sight
 
-def part_one(asteroids):
-	num_detections = set()
+
+def find_station(asteroids):
+	num_detections = {}
 	for asteroid in asteroids:
-		num_detections.add(num_detected(asteroid, asteroids))
-	return max(num_detections)
+		num_detections[asteroid] = (len(num_detected(asteroid, asteroids)))
 
+	station = max(num_detections, key=num_detections.get)
+	return station, num_detections[station]
+
+
+
+def part_two(asteroids, station):
+	asteroids_dict = num_detected(station, asteroids)
+	# sort all asteroids by angle and distance
+	all_angles = list(sorted(asteroids_dict.keys()))
+	for key, value in asteroids_dict.items():
+		asteroids_dict[key] = sorted(value, key=lambda x: x[0])
+
+	destroyed = 0
+	for angle in cycle(all_angles):
+		if details := asteroids_dict[angle]:
+			cur_asteroid = details.pop(0)
+			destroyed += 1
+			if destroyed == 200:
+				return cur_asteroid
 
 
 asteroids = read_input()
-print(part_one(asteroids))
+station, n = find_station(asteroids)
+
+print("Part one:", n)
+print("Station is at", station)
+print(part_two(asteroids, station))
