57b3e77ff9007a01ad8e21cab7d5f4fa4af9f267
Toby Boyne <tobyboyne@gmail.com>
2020-04-11 14:35:12 +0100

    Day 12 Part 1

--- aoc-2015/2015-12.py
+++ aoc-2015/2015-12.py
@@ -1,14 +1,26 @@
 import re
+from json import loads
 
 def read_input():
 	with open("day12.txt") as f:
 		return f.read()
 
 
-def part_one(json):
-	all_nums = re.findall(r'-?[0-9]+(?![0-9])', json)
+def part_one(string):
+	all_nums = re.findall(r'-?\d+', string)
 	return sum(map(int, all_nums))
 
 
-json = read_input()
-print(part_one(json))
+def hook(obj):
+	if "red" in obj.values():
+		return {}
+	else:
+		return obj
+
+def part_two(string):
+	as_json = str(loads(string, object_hook=hook))
+	return part_one(as_json)
+
+string = read_input()
+print(part_one(string))
+print(part_two(string))
