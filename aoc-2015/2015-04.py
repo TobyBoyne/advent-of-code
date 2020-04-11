73a2975e18d3161332ca059ca427223196166a3e
Toby Boyne <tobyboyne@gmail.com>
2020-04-12 00:02:51 +0100

    Day 4 Part 2

--- aoc-2015/2015-04.py
+++ aoc-2015/2015-04.py
@@ -6,4 +6,6 @@ for i in range(100000000):
 	hashed_string = md5((string + str(i)).encode()).hexdigest()
 	if hashed_string[:5] == '00000':
 		print("Part one", i)
+	if hashed_string[:6] == '000000':
+		print("Part two", i)
 		break
