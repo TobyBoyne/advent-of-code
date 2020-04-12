fd58a8fff7fe5573066993ad6a9f0c78ffc1b216
Toby Boyne <tobyboyne@gmail.com>
2020-04-12 13:33:24 +0100

    Day 23 Part 2

--- aoc-2016/2016-23.py
+++ aoc-2016/2016-23.py
@@ -1,3 +1,5 @@
+from math import factorial
+
 toggle = {
 	'inc': 'dec',
 	'dec': 'inc',
@@ -23,10 +25,15 @@ def as_value(registers, value):
 	return out
 
 
-def run_instructions(instructions, initial_reg):
+def run_instructions(instructions, initial_reg, print_at_zero=False):
 	registers = initial_reg
 	i = 0
 	while i < len(instructions):
+		if print_at_zero and i == 9 and registers['d'] == 0 and registers['a'] and registers['b'] > 1:
+			registers['b'] -= 1
+			registers['a'] *= registers['b']
+			print(i, registers)
+			continue
 		op, x, *y = instructions[i]
 		if op == 'tgl':
 			x = as_value(registers, x)
@@ -53,6 +60,18 @@ def part_one(instructions):
 	initial_reg = {'a': 7, 'b': 0, 'c': 0, 'd': 0}
 	return run_instructions(instructions, initial_reg)['a']
 
+def part_two_slow(instructions):
+	"""Too slow to find answer. Shows that each time d = 0, a = a * b, and b decreases by one
+	Therefore finds 12!
+	To find the extra constant, just use part_one - 7!
+	constant = 12703 - 5040"""
+	initial_reg = {'a':12, 'b':0, 'c':0, 'd':0}
+	return run_instructions(instructions, initial_reg, print_at_zero=True)
+
+def part_two():
+	return factorial(12) + 7663
 
 instructions = read_input()
 print(part_one(instructions))
+# print(part_two_slow(instructions))
+print(part_two())
