e432bd3bffa814ba83b2adb3079ab16fa000ca1d
Toby Boyne <tobyboyne@gmail.com>
2020-04-14 12:41:45 +0100

    Day 5 Part 2

--- aoc-2016/2016-05.py
+++ aoc-2016/2016-05.py
@@ -11,8 +11,18 @@ def part_one(string):
 				return password
 	return "Failed"
 
-
+def part_two(string):
+	password = ['' for _ in range(8)]
+	for i in range(25176242):
+		hashed_string = md5((string + str(i)).encode()).hexdigest()
+		if hashed_string[:5] == '00000':
+			pos = hashed_string[5]
+			if '0' <= pos <= '7' and not password[int(pos)]:
+				password[int(pos)] = hashed_string[6]
+				if all(password):
+					return ''.join(password)
 
 
 puzz_input = "ugkcyxxp"
 print(part_one(puzz_input))
+print(part_two(puzz_input))
