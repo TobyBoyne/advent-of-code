144540bcbf06ae339bbcc4f49eec8b714dd095f2
TobyBoyne <tobyboyne@gmail.com>
2021-12-19 17:56:35 +0000

    2021 Day 9 Part 2

--- aoc-2021/2021-09.py
+++ aoc-2021/2021-09.py
@@ -3,7 +3,7 @@ import numpy as np
 with open('day09.txt') as f:
     depth = np.array([[c for c in line.strip()] for line in f.readlines()], dtype=int)
 
-def part_one():
+def find_minima():
     depth_bordered = 10 * np.ones((depth.shape[0] + 2, depth.shape[0] + 2))
     depth_bordered[1:-1, 1:-1] = depth
 
@@ -14,7 +14,57 @@ def part_one():
     miny = np.logical_and(diffy < 0, np.roll(diffy, -1, axis=0) > 0)
     all_min = np.logical_and(minx, miny)
 
-    low_points = depth[np.nonzero(all_min)]
+    return all_min
+
+def part_one():
+    minima = find_minima()
+    low_points = depth[np.nonzero(minima)]
     return (low_points + 1).sum()
 
+dirs = np.array([
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+])
+def basin_size(basins, start_idx):
+    to_explore = [start_idx]
+    all_explored = set()
+    total_size = 0
+    while to_explore:
+        prev_to_explore = to_explore
+        to_explore = []
+
+        for pt in prev_to_explore:
+            # if out of bounds, a 9, or already explored
+            if tuple(pt) in all_explored or \
+                not (0 <= pt[0] < basins.shape[0]) or \
+                not (0 <= pt[1] < basins.shape[0]) or \
+                basins[tuple(pt)] == -1:
+
+                continue
+
+            all_explored.add(tuple(pt))
+
+            total_size += 1
+            next_pos = dirs + pt
+            to_explore += list(next_pos)
+
+    return total_size
+
+def part_two():
+    minima = find_minima()
+    basins = np.zeros_like(depth)
+    basins[np.nonzero(depth == 9)] = -1
+    min_idxs = np.nonzero(minima)
+
+    basin_sizes = []
+    for x, y in zip(*min_idxs):
+        start_idx = np.array([x, y])
+        basin_sizes.append(basin_size(basins, start_idx))
+
+    largest_basins = list(reversed(sorted(basin_sizes)))[:3]
+    return np.prod(largest_basins)
+
 print(part_one())
+print(part_two())
